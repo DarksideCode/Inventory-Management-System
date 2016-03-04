569732da05afb6d81400a16dcb75a7e4b6b071ed
Henry Martens <henry_martens@outlook.de>
2016-03-04 21:33:56 +0100

    [IMS-?] Refactoring

--- InventoryManagementSystem/InventoryManagementSystem/dataAccess/ProcessorDataAccess.cs
+++ InventoryManagementSystem/InventoryManagementSystem/dataAccess/ProcessorDataAccess.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using InventoryManagementSystem.components;
+﻿using InventoryManagementSystem.components;
 
 namespace InventoryManagementSystem.DB_Models
 {
--- InventoryManagementSystem/InventoryManagementSystem/dataAccess/ProducerDataAccess.cs
+++ InventoryManagementSystem/InventoryManagementSystem/dataAccess/ProducerDataAccess.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using InventoryManagementSystem.components;
 using InventoryManagementSystem.utilitys;
 using MySql.Data.MySqlClient;
