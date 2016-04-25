e4f1c5ca1a3ff72b7b2c5ca1250630377ff8a402
HenryMartens <martens_henry@web.de>
2016-04-25 16:43:44 +0200

    [IMS-?] Refactoring & Styling

--- InventoryManagementSystem/InventoryManagementSystem/presentation/EditPhysicalInterfaces.xaml.cs
+++ InventoryManagementSystem/InventoryManagementSystem/presentation/EditPhysicalInterfaces.xaml.cs
@@ -30,6 +30,11 @@ namespace InventoryManagementSystem.presentation
             list = usedEntities;
         }
 
+        /// <summary>
+        /// Füllt die Liste der verwendeten und nicht verwendeten Schnittstellen auf, damit diese auf
+        /// der Oberfläche zu sehen sind.
+        /// </summary>
+        /// <param name="usedEntities"></param>
         private void FillLists(List<PhysicalInterfaceWithCount> usedEntities)
         {
             PhysicalInterfaceDataAccess dataAccess = new PhysicalInterfaceDataAccess();
@@ -49,6 +54,10 @@ namespace InventoryManagementSystem.presentation
             }
         }
 
+        /// <summary>
+        /// Wird aufgerufen, wenn auf den Button mit dem '+' geklickt wird.
+        /// Fügt das selektierte Item zur Liste der verwendeten Schnittstellen hinzu. 
+        /// </summary>
         private void Add_Click(object sender, RoutedEventArgs e)
         {
             if(this.physicalInterfaceList.SelectedItems.Count > 0)
@@ -58,6 +67,10 @@ namespace InventoryManagementSystem.presentation
             }
         }
 
+        /// <summary>
+        /// Wird aufgerufen, wenn auf den Button mit dem '-' geklickt wird.
+        /// Entfernt die selektierte Schnittstelle aus der Liste der verwendeten Schnittstellen.
+        /// </summary>
         private void Remove_Click(object sender, RoutedEventArgs e)
         {
             if(this.usedList.SelectedItems.Count > 0)
@@ -67,12 +80,22 @@ namespace InventoryManagementSystem.presentation
             }
         }
 
+        /// <summary>
+        /// Wird aufgerufen, wenn auf den Speichern-Button geklickt wird.
+        /// Speichert die aktuelle Liste und schließt das Fenster.
+        /// </summary>
         private void Save_Click(object sender, RoutedEventArgs e)
         {
             List<PhysicalInterfaceWithCount> list = this.BuildList();
             this.Close();
         }
 
+        /// <summary>
+        /// Generiert eine Liste von 'PhysicalInterfaceWithCount' aus der Tabelle der
+        /// verwendeten Schnittstellen. Es werden die Schnittstellen und Anzahl der Vorkommen
+        /// ermittelt und in die Liste geschrieben.
+        /// </summary>
+        /// <returns>Liste von PhysicalInterfaceWithCount</returns>
         private List<PhysicalInterfaceWithCount> BuildList()
         {
             PhysicalInterfaceDataAccess dataAccess = new PhysicalInterfaceDataAccess();
@@ -97,6 +120,15 @@ namespace InventoryManagementSystem.presentation
             }
 
             return list;
+        }
+
+        /// <summary>
+        /// Wird aufgerufen, wenn auf den Abbrechen-Button geklickt wird.
+        /// Schließt den aktuellen Dialog, ohne die Schnittstellen zu speichern.
+        /// </summary>
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
